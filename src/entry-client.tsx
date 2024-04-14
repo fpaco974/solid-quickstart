9d5da45788f401ce5e934cfe8c2e213bf4dfe19f
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2024-04-14 19:08:51 +0400

    Update entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,22 @@
+import { onMount } from 'solid-js';
 import { mount, StartClient } from "solid-start/entry-client";
 
-mount(() => <StartClient />, document);
+mount(() => {
+  onMount(() => {
+    document.querySelectorAll('a').forEach((button) => {
+      button.addEventListener('click', function (event) {
+        event.preventDefault();
+        var url = button.getAttribute('href');
+        window.location.href = url;
+      });
+
+      button.addEventListener('touchstart', function (event) {
+        event.preventDefault();
+        var url = button.getAttribute('href');
+        window.location.href = url;
+      });
+    });
+  });
+
+  return <StartClient />;
+}, document);
